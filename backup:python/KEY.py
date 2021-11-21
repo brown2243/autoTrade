c0422e244284056a7926b56dbb222401c28a391b
brown <brown2243@naver.com>
2021-11-21 15:48:18 +0900

    [chore] python 제거

---
+++ backup:python/KEY.py
@@ -0,0 +1,8 @@
+def key(key):
+    APIKEY = "GcYJCPVNv5X8SXzOG35kxMsTjG0bT5XnJ94c56nM5OVWZDHAtUrmjFTue3xVYVGP"
+    SCTKEY = "e1LbUMxw0Ucg7FJveHf2u7lOs1CAusKtkZ2YE9Saf3WJgjdKGJUfx0ENwXSYEXd7"
+    if(key == "api"):
+        return APIKEY
+    elif(key == "sct"):
+        return SCTKEY
+
